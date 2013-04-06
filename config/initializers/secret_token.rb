fcf989870ce0d4010b94a93b7f88a323050928d5
Lars Walen <lars@walen.me>
2013-04-06 00:20:21 -0600

    Added rails boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectMgmt::Application.config.secret_token = 'd36d1ed2377914dac64636ebaeb6ed6fca314953b9590a7ad4e9b2b235bd9a68fb7c9cd1cc4097bdf16597615d7c732f7043472569dea4c01b75cb5556d8df59'
